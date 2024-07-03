2e484600ee2c4256ea8905c409d523394e0bc51f
Chukwuma Ebube <chukwuma619@gmail.com>
2024-07-04 00:13:46 +0100

    add speed insight and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -39,6 +40,8 @@ export default function RootLayout({
         <Providers>
           <div className="w-full">{children}</div>
         </Providers>
+        <SpeedInsights />
+          <Analytics/>
       </body>
     </html>
   );
