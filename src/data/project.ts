bdd846d918b719e07ac263d2d82430ba4ef47bd8
Chukwuma Ebube <chukwuma619@gmail.com>
2024-07-06 12:20:29 +0100

    update project url

--- src/data/project.ts
+++ src/data/project.ts
@@ -10,14 +10,14 @@ export const projects = [
       name: "My Portfolio Website",
       description:
         "Showcasing my projects and achievements with a sleek, personal touch.",
-      link: { href: "#", label: "github.com" },
+      link: { href: "https://github.com/chukwuma619/personal-portfolio", label: "github.com" },
        logo: "/profile-image.JPG",
     },
     {
       name: "Backend-Fanbuck",
       description:
         "Empowering individuals to raise funds for personal projects through secure, anonymous donations.",
-      link: { href: "#", label: "github.com" },
+      link: { href: "https://github.com/chukwuma619/fanbucks_backend", label: "github.com" },
        logo: "/fanbuck.png",
     },
    
